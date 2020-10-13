e803b2d10823e46374e78dc365828f74beb6b744
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2020-10-13 15:48:11 +0300

    Common.js to ESM

--- src/components/ComponentA.vue
+++ src/components/ComponentA.vue
@@ -1,16 +1,20 @@
 <template>
-  <button class="p-4 border bg-gray-100 rounded-lg mb-12" @click.prevent="sayHello">Say Hello to Component B!</button>    
+  <button
+    class="p-4 border bg-gray-100 rounded-lg mb-12"
+    @click.prevent="sayHello"
+  >
+    Say Hello to Component B!
+  </button>
 </template>
 
 <script>
-const EventBus = require('../bus');
+import EventBus from '../bus';
 
-module.exports = {
+export default {
   methods: {
     sayHello: function () {
       EventBus.$emit('say-hello');
-    }
-  }
-
-}
+    },
+  },
+};
 </script>
